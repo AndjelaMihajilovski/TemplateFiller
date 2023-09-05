b115b4aec90b97ca2028288c0c2a333114c407b5
Andjela <andjela.mihajilovski@gmail.com>
2023-09-05 11:08:05 +0200

    added validation and notifications

--- TemplateFiller.py
+++ TemplateFiller.py
@@ -13,7 +13,9 @@ csvPath = ""
 path = ""
 
 def BrowseFile (): 
-    global filename 
+    global filename
+    global filenameNotification
+    filenameNotification.config(text='') 
     currdir = os.getcwd()
     filetypes = (("Word files", "*.docx"), ("All files", "*.*"))
     tempdir = fd.askopenfilename(parent=root, initialdir=currdir, title='Please select a Word Template File', filetypes=filetypes)
@@ -22,12 +24,14 @@ def BrowseFile ():
         print("You chose %s" % tempdir)
         filename = tempdir
         print ("\template file name = ", filename)
-        label1 = prog.Label(root, text= filename, fg='green', font=('helvetica', 10, 'bold'))
-        canvas1.create_window(250, 120, window=label1)
+        filenameLabel = prog.Label(root, text= filename, fg='green', font=('helvetica', 10, 'bold'))
+        canvas1.create_window(250, 120, window=filenameLabel)
 
 def BrowseCSV (): 
     global csvPath 
     currdir = os.getcwd()
+    global csvNotification
+    csvNotification.config(text='')
     filetypes = (("CSV files", "*.csv"), ("All files", "*.*"))
     tempdir = fd.askopenfilename(parent=root, initialdir=currdir, title='Please select a CSV file', filetypes=filetypes)
     
@@ -35,11 +39,13 @@ def BrowseCSV ():
         print("You chose %s" % tempdir)
         csvPath = tempdir
         print ("\template file name = ", csvPath)
-        label1 = prog.Label(root, text= csvPath, fg='green', font=('helvetica', 10, 'bold'))
-        canvas1.create_window(250, 170, window=label1)
+        csvPathLabel = prog.Label(root, text= csvPath, fg='green', font=('helvetica', 10, 'bold'))
+        canvas1.create_window(250, 170, window=csvPathLabel)
 
 def BrowseDestinationPath (): 
     global path 
+    global pathNotification
+    pathNotification.config(text='')
     currdir = os.getcwd()
     tempdir = fd.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
     
@@ -47,33 +53,49 @@ def BrowseDestinationPath ():
         print("You chose %s" % tempdir)
         path = tempdir
         print ("\template file name = ", path)
-        label1 = prog.Label(root, text= path, fg='green', font=('helvetica', 10, 'bold'))
-        canvas1.create_window(250, 220, window=label1)
+        pathLabel = prog.Label(root, text= path, fg='green', font=('helvetica', 10, 'bold'))
+        canvas1.create_window(250, 220, window=pathLabel)
 
 def CreateTemplate ():
     global filename
     global csvPath 
     global path 
-    with open(csvPath, newline='') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-        headers = csv_reader.fieldnames
-        result_dir = os.path.join(path, "result")
-        if not os.path.exists(result_dir):
-            os.makedirs(result_dir)
-            print("The 'result' directory is created!")
+    global filenameNotification
+    global csvNotification
+    global pathNotification
 
-        for row in csv_reader:
-            doc = Document(filename)
-            print('Parsing: ' + row['Name'])
+    if  filename == "":
+        filenameNotification = prog.Label(root, text= 'You didn`t choose Word Template file!', fg='green', font=('helvetica', 12, 'bold'))
+        canvas1.create_window(250, 270, window=filenameNotification)
 
-            for columnName in headers:
-                for paragraph in doc.paragraphs:
-                    paragraph.text = paragraph.text.replace('<' + columnName + '>', row[columnName])
+    if  path == "":
+        pathNotification = prog.Label(root, text= 'Tou didn`t choose save directory!', fg='green', font=('helvetica', 12, 'bold'))
+        canvas1.create_window(250, 310, window=pathNotification)  
 
-            doc.save(os.path.join(result_dir, row[headers[0]] + '.docx'))     
+    if csvPath != "": 
+        with open(csvPath, newline='') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            headers = csv_reader.fieldnames
+            result_dir = os.path.join(path, "result")
+            if not os.path.exists(result_dir):
+                os.makedirs(result_dir)
+                print("The 'result' directory is created!")
 
-        label1 = prog.Label(root, text= 'Finished!', fg='green', font=('helvetica', 12, 'bold'))
-        canvas1.create_window(250, 270, window=label1)
+            for row in csv_reader:
+                doc = Document(filename)
+                print('Parsing: ' + row['Name'])
+
+                for columnName in headers:
+                    for paragraph in doc.paragraphs:
+                        paragraph.text = paragraph.text.replace('<' + columnName + '>', row[columnName])
+
+                doc.save(os.path.join(result_dir, row[headers[0]] + '.docx'))     
+
+                label1 = prog.Label(root, text= 'Finished!', fg='green', font=('helvetica', 12, 'bold'))
+                canvas1.create_window(250, 270, window=label1)
+    else:
+        csvNotification = prog.Label(root, text= 'You didn`t choose CSV file!', fg='green', font=('helvetica', 12, 'bold'))
+        canvas1.create_window(250, 290, window=csvNotification)  
     
 button = prog.Button(text='OpenTemplate', command=BrowseFile, bg='grey',fg='white')
 canvas1.create_window(250, 100, window=button)
